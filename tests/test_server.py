ca7ea3ff8326b8b31b4b6a1c69785bb68a2aa253
Jinsol Kim <jinsolkim91@gmail.com>
2024-12-03 17:44:51 +0800

    test code updated

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,10 +1,10 @@
 from fastapi.testclient import TestClient
-from server import app
 import sys
 import os
 
 # Add the parent directory to the Python path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from server import app
 
 client = TestClient(app)
 
